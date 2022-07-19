cf1128da39185974c9919a9342729cdce9a03228
Jay Slade <brandonslade2@gmail.com>
2022-07-18 18:58:24 -0700

    Functions 2 Finished

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,8 +18,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers = mixedNumbers.filter(element => element % 2 === 0) //.filter will create anothe array on this variable.
 
+console.log(evenNumbers)
 
 
 ////////// PROBLEM 2 //////////
@@ -39,9 +40,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+const postTaxPrices = prices.map(e => e * 1.07);
 
-
+console.log(postTaxPrices)
 
 ////////// PROBLEM 3 //////////
 
@@ -52,12 +53,15 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 /*
   Use the reduce method to calculate the sum of all the populations in the array.
   The reduce function has a slightly different setup for its callback.
-  function(runningTotal, curElement, curIndex, wholeArray){} Function Form
+  function(runningTotal, curElement, curIndex, wholeArray){} Function Form  /accumulator is the wrong word for this from the slides. runningtotal is so much better. but more specifically, runningelement would do it
   (runningTotal, curElement, curIndex, wholeArray)=>{} Arrow Form
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+ const totalPopulation = populations.reduce((acc, curr) => acc + curr) //taking anonymous callback function as the running total
+
+
+ console.log(totalPopulation)
 
 
 
@@ -82,9 +86,13 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+ const myStrongest = monstersInYourPocket.filter(element => {
+  if (element.CP > 200) {
+    return element.monster
+  }
+  })
 
-
+console.log(myStrongest)
 
 ////////// PROBLEM 5 //////////
 
@@ -99,7 +107,9 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-// CODE HERE
+const totalsaftertax = orders.map(element => element.price * (element.tax+1))
+
+console.log(totalsaftertax)
 
 
 
@@ -119,4 +129,8 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method(s) to get the sum of bobsTotal.
 */
 
-// CODE HERE
+const bobsTotal = purchases
+.filter(e => e.owner === "Bob")
+.reduce((a, c) => a + c.price, 0)
+
+console.log(bobsTotal)
